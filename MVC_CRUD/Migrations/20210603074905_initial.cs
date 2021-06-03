ff76bb23f2fc9e0c74d46fca49f1fa3655080aba
Crister <Torresykes09@gmail.com>
2021-06-03 16:42:15 +0800

    Add project files.

---
+++ MVC_CRUD/Migrations/20210603074905_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_CRUD.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    mName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    lName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    gender = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    emailAddress = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    contactNo = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ MVC_CRUD/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string fName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string mName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string lName { get; set; }
+        [Required]
+        [StringLength(8)]
+        public string gender { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string address { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string emailAddress { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string contactNo { get; set; }
+    }
+}
